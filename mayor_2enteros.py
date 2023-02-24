4ca05b9d00b9dd74bd66ae10d81dee18cc39c917
laurarojas1456 <laurarojas@colegioguanenta.edu.co>
2023-02-24 07:22:26 -0500

    Se creó el programa y se agregó al README

--- mayor_2enteros.py
+++ mayor_2enteros.py
@@ -9,12 +9,12 @@ x = int(input("digite el valor de x: "))
 y = int(input("digite el valor de y: "))
 
 # processing
-if x > y:
+if x == y:
     # output
     print("los numeros son iguales...")
 else:
     if x > y:
-    mayor = x
-else:
-    mayor = y
+        mayor = x
+    else:
+        mayor = y
     print ("El mayor entre " + str(x) + " y " + str(y) + " es " + str(mayor))
